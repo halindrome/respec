7a495f7e3b4cf09d1e3b2b764a470a67a72ad9ca
Shane McCarron <halindrome@gmail.com>
2014-03-27 19:38:52 -0500

    Initial version to support permalinks.

---
+++ js/w3c/permalinks.js
@@ -0,0 +1,41 @@
+// Module w3c/permalinks
+// Adds "permalinks" into the document at sections with explicit IDs
+// Introduced by Shane McCarron (shane@aptest.com) from the W3C PFWG
+// Only enabled when the includePermalinks option is set to true.
+// Defaults to false.
+
+define(
+    ["core/utils"], // load this to be sure that the jQuery extensions are loaded
+    function (utils) {
+        return {
+            run:    function (conf, doc, cb, msg) {
+                if (conf.includePermalinks === true ) {
+                    msg.pub("start", "w3c/permalinks");
+                    var $secs = $(doc).find("h2, h3, h4, h5, h6");
+                    $secs.each(function(i, item) {
+                        var $item = $(item)
+                        ,   resourceID = $item.attr('id');
+
+                            var par = $item.parent()[0];
+                            if (par && par.localName && par.localName.toLowerCase() == "section") {
+                                if (!$(par).hasClass("introductory")) {
+                                    resourceID = $(par).attr('id') ;
+                                } else {
+                                    resourceID = null;
+                                }
+                            }
+
+                        // if we still have resourceID
+                        if (resourceID != null) {
+                            // we have an id.  add a permalink
+                            // right after the h* element
+                            $item.after("<p class='permalink'><a href='#"+resourceID+"' title='Permalink for "+resourceID+"'>#</a></p>");
+                        }
+                    });
+                    msg.pub("end", "w3c/permalinks");
+                };
+                cb();
+            }
+        };
+    }
+);
